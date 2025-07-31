e09a5e25f2860e7d53743cc2d5bfe71566bf61ac
t3201v <truong1320vv@gmail.com>
2025-07-31 22:33:51 +0700

    update: sqlc

--- identity/internal/db/schema/migrations/20250724031539_init_schema.sql
+++ identity/internal/db/schema/migrations/20250724031539_init_schema.sql
@@ -1,9 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-
+CREATE TABLE users (
+    id uuid primary key
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-
+DROP TABLE users;
 -- +goose StatementEnd
